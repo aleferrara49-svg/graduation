909502467db0205b568d523175a9bf1eed801be4
aleferrara49-svg <aleferrara49@gmail.com>
2026-07-12 03:24:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import { 
+    getStorage 
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzQ1Igp_eCFjyvFYfHSSl_OCgBfOWK1jg",
+  authDomain: "laurea-di-alessandra.firebaseapp.com",
+  projectId: "laurea-di-alessandra",
+  storageBucket: "laurea-di-alessandra.firebasestorage.app",
+  messagingSenderId: "377947615238",
+  appId: "1:377947615238:web:2d81f2390ddd8224129387"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
+
+
+console.log("Firebase collegato 🎓");
+
+
 const camera = document.getElementById("cameraInput");
 const gallery = document.getElementById("galleryInput");
 
 const preview = document.getElementById("preview");
-const uploadButton = document.getElementById("uploadButton");
 
-const message = document.getElementById("message");
+const uploadButton = document.getElementById("uploadButton");
 
 
 function showPhoto(file) {
@@ -29,21 +53,10 @@ function showPhoto(file) {
 
 
 camera.addEventListener("change", function() {
-
     showPhoto(this.files[0]);
-
 });
 
 
 gallery.addEventListener("change", function() {
-
     showPhoto(this.files[0]);
-
-});
-
-
-uploadButton.addEventListener("click", function() {
-
-    message.innerHTML = "Foto pronta per essere caricata 📸";
-
 });
